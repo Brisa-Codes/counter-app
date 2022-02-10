e0bca363f141d20c25a4a851215894c629c106a3
Brisa-Codes <brisamukunde1@gmail.com>
2022-02-10 17:20:23 +0300

    Update: Functionalities

---
+++ script.js
@@ -0,0 +1,39 @@
+//variables -- add, subtract, reset
+
+const count = document.querySelector(".count");
+
+const add = document.querySelector(".add");
+
+const sub = document.querySelector(".subtract");
+
+const resetNum = document.querySelector(".reset");
+
+add.addEventListener("click", function() {
+    // console.log("this is adding");
+    count.innerHTML++;
+    setColor();
+   //document.getElementById("increase-text").style.color = "blue";
+})
+
+sub.addEventListener("click", function() {
+    // console.log("this is subtracting");
+    count.innerHTML--;
+    setColor();
+})
+
+resetNum.addEventListener("click", function() {
+    //console.log("this is reset")
+    count.innerHTML = 0;
+    setColor();
+})
+
+function setColor() {
+    if (count.innerHTML > 0) {
+        count.style.color = "green";
+    } else if(count.innerHTML < 0) {
+        color.style.color = "yellow";
+    } else {
+        count.style.color = "black";
+    }
+}
+

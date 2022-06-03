0cecb16d89ba6dbd4da0295d9d29149c9ea64944
Brisa-Codes <brisamukunde1@gmail.com>
2022-06-03 10:59:47 +0300

    docker test

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ const app = express();
 
 app.get('/', (req, res) => {
     res.send('Happy Hacking!');
-    }
-);
+});
 
-app.listen(5000, () => {
-    console.log('Server is running on port 5000');
+app.listen(4000, () => {
+    console.log('Server is running on port 4000');
 });
